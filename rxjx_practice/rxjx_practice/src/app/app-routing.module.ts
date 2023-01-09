4a9bc872e27a59b0be0aa62d9494e299a6d62951
nishaivy <113030466+nishaivy@users.noreply.github.com>
2023-01-09 12:11:26 +0530

    promise

---
+++ rxjx_practice/rxjx_practice/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PromiseComponent } from './promise/promise.component';
+
+const routes: Routes = [
+  {
+    path:'promise',
+    component:PromiseComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
